946d7ebb7934903b714f2e821c1251074d1b9bd8
aqariio <aquario.mw@gmail.com>
2024-11-19 11:48:18 -0500

    Add funny animation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 function App() {
+  const addData = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "newcollection"), {
+        field: "amongus"
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I sure love learning react
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={addData}
         >
-          Learn React
-        </a>
+          Add Data Among Us
+        </button>
       </header>
     </div>
   );
