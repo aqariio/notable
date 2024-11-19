41e526fcf970a7ae0900ecd31ab52dbfb05c3c60
aqariio <aquario.mw@gmail.com>
2024-11-19 12:54:53 -0500

    Fix firebase deployment using wrong folder

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  // apiKey: process.env.API_KEY,
+  // authDomain: process.env.AUTH_DOMAIN,
+  // projectId: process.env.PROJECT_ID,
+  // storageBucket: process.env.STORAGE_BUCKET,
+  // messagingSenderId: process.env.SENDER_ID,
+  // appId: process.env.APP_ID
+  apiKey: "AIzaSyAjzwfnycL-s4HAZopwD9tJSH72q-4POE0",
+  authDomain: "notable-max.firebaseapp.com",
+  projectId: "notable-max",
+  storageBucket: "notable-max.firebasestorage.app",
+  messagingSenderId: "916202186075",
+  appId: "1:916202186075:web:870f31559bb5e2e5eae67e"
 };
 
 // Initialize Firebase
